64f011b3b7489b860b3606b6d26fc734503cc03e
albertofcyber98 <yohanesalbert2@gmail.com>
2022-05-06 06:12:10 +0800

    Update Index V1

--- src/components/button/button-primary/index.js
+++ src/components/button/button-primary/index.js
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom'
 const ButtonPrimary = () => {
     return (
         <Link to='/'>
-            <button className='mt-10 px-10 text-base md:text-lg py-3 bg-[#00BC9A] text-white rounded filder:drop-shadow(0 2px 10px rgb(1,188,154/0.2)) hover:bg-[#049A7F] transition-all duration-1000'>
+            <button className='mt-10 px-8 sm:px-10 text-base md:text-lg py-3 bg-[#00BC9A] text-white rounded filder:drop-shadow(0 2px 10px rgb(1,188,154/0.2)) hover:bg-[#049A7F] transition-all duration-1000'>
                 Let's work                
             </button>
         </Link>
--- src/components/button/button-secondary/index.js
+++ src/components/button/button-secondary/index.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const ButtonSecondary = () => {
     return(
         <Link to='/portofolio'>
-            <button className='mt-10 px-10 py-3 text-base md:text-lg bg-[#868686] text-white rounded ml-6 hover:bg-[#646464] transition-all duration-1000'>
+            <button className='mt-10 px-6 sm:px-10 py-3 text-base md:text-lg bg-[#868686] text-white rounded ml-6 hover:bg-[#646464] transition-all duration-1000'>
                 View my work               
             </button>
         </Link >
